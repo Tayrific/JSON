c646e4c4bbf9c21c48c47a13416a1a672b189fdf
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-31 10:58:13 +0100

    Using System.Dynamic and JsonConverter

---
+++ ExpandoObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Dynamic;
+using Newtonsoft.Json;
+
+namespace DynamicExpando
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dynamic and ExpandoObjects");
+            Console.WriteLine("--------------------------");
+            dynamic author = new ExpandoObject();
+            author.Name = "Xavier Morera";
+            author.Courses = new List<string>() { "Solr", "Spark", "Python" };
+            author.Happy = true;
+
+            Console.WriteLine("--Serialise--");
+
+            string json = JsonConvert.SerializeObject(author, Formatting.Indented);
+            Console.WriteLine(json);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("--Deserialise--");
+            author = JsonConvert.DeserializeObject(json);
+            Console.WriteLine(author);
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("---Output authors name---");
+            Console.WriteLine("Name: " + author.Name);
+
+        }
+    }
+}
---
+++ SerialiseWithJsonConverters/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SerialiseWithJsonConverters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<StringComparison> stringComparisons = new List<StringComparison>
+            {
+                StringComparison.CurrentCulture,
+                StringComparison.Ordinal
+            };
+
+            //output
+            foreach (StringComparison stringComparison in stringComparisons)
+            {
+                Console.Write(stringComparison + " ");
+            }
+            Console.WriteLine("\n------------------------");
+
+            //Serialise without converter
+            string jsonWithoutConverter = JsonConvert.SerializeObject(stringComparisons);
+            Console.WriteLine(jsonWithoutConverter);
+            Console.WriteLine("------------------------");
+
+            //Serialise with converter
+            string jsonWithConverter = JsonConvert.SerializeObject(stringComparisons, new StringEnumConverter());
+            Console.WriteLine(jsonWithConverter);
+            Console.WriteLine("------------------------");
+
+            //Deserialise
+            List<StringComparison> newStringComparsions = JsonConvert.DeserializeObject<List<StringComparison>>(jsonWithConverter, new StringEnumConverter());
+            Console.WriteLine(string.Join(", ", newStringComparsions.Select(c => c.ToString()).ToArray()));
+        }
+    }
+}
