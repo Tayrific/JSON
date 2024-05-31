8c338101004f5a339987cd2735755eed8da2b422
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-31 10:26:06 +0100

    Serialise JSON to a file

---
+++ SerialiseJSONToFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace SerialiseJSONToFile
+{
+    internal class Program
+    {
+        public class Movie
+        {
+            public string Name { get; set; }
+            public int Year { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie
+            {
+                Name = "Bad Boys",
+                Year = 1995
+            };
+            Movie movie2 = new Movie
+            {
+                Name = "Super Mario Bros",
+                Year = 1993
+            };
+
+
+            File.WriteAllText(@"movie.json", JsonConvert.SerializeObject(movie) + JsonConvert.SerializeObject(movie2));
+            
+        }
+    }
+}
